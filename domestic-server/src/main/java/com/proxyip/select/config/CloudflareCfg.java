43886c6f82cba62625ddaafecde2098669666d9c
fanyuhui <852221040@qq.com>
2024-03-29 19:20:42 +0800

    update code structure

---
+++ domestic-server/src/main/java/com/proxyip/select/config/CloudflareCfg.java
@@ -0,0 +1,54 @@
+package com.proxyip.select.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>
+ * CloudflareCfg
+ * </p >
+ *
+ * @author yuhui.fan
+ * @since 2024/2/22 18:14
+ */
+@Configuration
+@ConfigurationProperties(prefix = "cloudflare-cfg")
+public class CloudflareCfg {
+
+    private String zoneId;
+    private String apiToken;
+    private String rootDomain;
+    private String proxyDomainPrefix;
+
+    public String getRootDomain() {
+        return rootDomain;
+    }
+
+    public void setRootDomain(String rootDomain) {
+        this.rootDomain = rootDomain;
+    }
+
+    public String getProxyDomainPrefix() {
+        return proxyDomainPrefix;
+    }
+
+    public void setProxyDomainPrefix(String proxyDomainPrefix) {
+        this.proxyDomainPrefix = proxyDomainPrefix;
+    }
+
+    public String getZoneId() {
+        return zoneId;
+    }
+
+    public void setZoneId(String zoneId) {
+        this.zoneId = zoneId;
+    }
+
+    public String getApiToken() {
+        return apiToken;
+    }
+
+    public void setApiToken(String apiToken) {
+        this.apiToken = apiToken;
+    }
+}
